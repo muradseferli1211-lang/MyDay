91cc9b2e6182df103b8460d442ac50990c59d789
muradseferli1211-lang <muradseferli1211@gmail.com>
2026-09-23 15:10:58 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,5 +28,5 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
-    implementation("androidx.glance:glance-appwidget:1
+    implementation("androidx.glance:glance-appwidget:1.1.1")
+}
